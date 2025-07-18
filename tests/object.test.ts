6dcab2ff7aebcec198248552062484204b534915
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-07-18 14:03:19 +0700

    [basic]: object type

---
+++ tests/object.test.ts
@@ -0,0 +1,20 @@
+describe('Object', function() { 
+  it('should support in typescript', function() {
+
+    const person: {id: string, name: string} = {
+      id: "1",
+      name: "wahyu"
+    }
+
+    console.info(person)
+
+    person.id = "w002"
+    person.name = "Hadjar"
+
+    // Property 'gender' does not exist on type '{ id: string; name: string; }'.
+    // person.gender = "Male"
+
+    console.info(person.name)
+
+  })
+})
