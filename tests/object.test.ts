8d92559317db69fcf789f1f765c145e0ad9606f7
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-07-21 11:22:53 +0700

    [basic]: function interface

--- tests/object.test.ts
+++ tests/object.test.ts
@@ -17,4 +17,18 @@ describe('Object', function() {
     console.info(person.name)
 
   })
+
+  it('should support function interface', function() {
+    
+    interface AddFunction {
+      (value1: number, value2: number): number
+    }
+
+    const add: AddFunction = (value1: number, value2: number) : number => {
+      return value1 + value2
+    }
+
+    expect(add(3,3)).toBe(6)
+
+  })
 })
