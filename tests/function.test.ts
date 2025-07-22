d653814041c95a14d090c92decd3e068ddb516b4
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-07-22 13:06:36 +0700

    [basic]: function parameter

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -14,4 +14,39 @@ describe('Function', function() {
     printHello("Wahyu")
 
   })
+
+  it('should support default value', function() {
+    function sayHello(name: string = "Guest"): string {
+      return `Hello ${name}`
+    }
+
+    expect(sayHello()).toBe("Hello Guest")
+    expect(sayHello("Fajar")).toBe("Hello Fajar")
+  })
+
+  it('should support rest parameter', function() {
+    function sum(...values: number[]): number {
+      let total = 0
+      for(const value of values) {
+        total += value
+      }
+      return total
+    }
+
+    expect(sum(1, 2, 3, 4, 5)).toBe(15)
+  })
+
+  it('should support optional parameter', function() {
+    function sayHello(firstName: string, lastName? : string): string {
+      if(lastName) {
+        return `Hello ${firstName} ${lastName}`
+      } else {
+        return `Hello ${firstName}`
+      }
+    }
+
+    expect(sayHello("Fajar")).toBe("Hello Fajar")
+    expect(sayHello("Haji", "Boim")).toBe("Hello Haji Boim")
+  })
+
 })
